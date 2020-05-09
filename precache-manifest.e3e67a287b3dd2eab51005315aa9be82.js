4609c4b6e43a3368d3d194ebab8cb5c2256f8fab
four778 <64983412+mrfireice@users.noreply.github.com>
2020-05-09 15:20:58 +0400

    Updates

---
+++ precache-manifest.e3e67a287b3dd2eab51005315aa9be82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78418d1602721c3cf303cd6604d9f0c",
+    "url": "/stiki/index.html"
+  },
+  {
+    "revision": "ec57951c8557c3477d14",
+    "url": "/stiki/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "c3f15dc551cf63eb2bf5",
+    "url": "/stiki/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ec57951c8557c3477d14",
+    "url": "/stiki/static/js/2.57d34642.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/stiki/static/js/2.57d34642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f15dc551cf63eb2bf5",
+    "url": "/stiki/static/js/main.04008a6e.chunk.js"
+  },
+  {
+    "revision": "79035b8ad69a4d3f0cf9",
+    "url": "/stiki/static/js/runtime-main.ef592aba.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/stiki/static/media/persik.4e1ec840.png"
+  }
+]);
